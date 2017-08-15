75d1b336fedef8b817804d82e5d5032cb8992d7f
Dmitry Mozzherin <dmozzherin@gmail.com>
2017-08-15 16:03:12 -0500

    prepare tables

--- lib/migrator.rb
+++ lib/migrator.rb
@@ -6,11 +6,13 @@ require 'dbd/Jdbc'
 require 'jdbc/mysql'
 require 'json'
 require 'csv'
+require 'redis'
 require_relative 'migrator/config'
 require_relative 'migrator/dumper'
 require_relative 'gnparser-assembly-0.4.1.jar'
 
-java_import "org.globalnames.parser.ScientificNameParser"
+java_import 'org.globalnames.parser.ScientificNameParser'
+java_import 'org.globalnames.UuidGenerator'
 
 # Migrates data from GNI to GNINDEX
 module Migrator
--- lib/migrator/dumper.rb
+++ lib/migrator/dumper.rb
@@ -3,21 +3,212 @@ module Migrator
   class Dumper
     def initialize
       @snp = ScientificNameParser.instance
+      @redis = Redis.new
       gni = Conf[:gni]
       Jdbc::MySQL.load_driver
       @db = DBI.connect(
         "DBI:Jdbc:mysql://#{gni[:host]}/#{gni[:database]}",
         gni[:username], gni[:password],
         'driver' => 'com.mysql.jdbc.Driver')
-
+      prepare_csv
     end
 
     def run
+      prepare_data_sources
+      prepare_name_strings
+      prepare_name_string_indices
+      prepare_vernacular_strings
+      prepare_vernacular_string_indices
+    end
+
+    def processing_title(table)
+      puts
+      puts "*" * 50
+      puts "Processing #{table}"
+      puts "*" * 50
+      puts
+    end
+
+    def prepare_data_sources
+      res = @db.execute "SELECT id, title, description,
+                          logo_url, web_site_url, data_url,
+                          refresh_period_days, name_strings_count,
+                          data_hash, unique_names_count, created_at,
+                          updated_at
+                          FROM data_sources"
+      while row = res.fetch do
+        entry = entry_data_source(row)
+        @data_sources << entry
+      end
+      res.finish
+    end
+
+    def entry_data_source(row)
+      [row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7],
+       row[8], row[9], row[10], row[11]]
+    end
+
+    def prepare_name_strings
+      processing_title("name_strings and Co")
+      offset = 0
+      limit = 10000
+      while offset < 10_000 # 26_000_000
+        puts "name_strings rows so far: #{offset}"
+        res = @db.execute "SELECT name, surrogate, id
+                           FROM name_strings
+                           limit #{limit} offset #{offset}"
+        offset += limit
+        while row = res.fetch do
+          entry = entry_name_strings(row)
+          @name_strings << entry
+        end
+        res.finish
+      end
+    end
+
+    def entry_name_strings(row)
+      name = row[0]
+      surrogate = row[1]
+      parsed = JSON.parse(@snp.fromString(name).renderCompactJson,
+                          symbolize_names: true)
+      id = parsed[:name_string_id]
+      canonical = canonical_uuid = nil
+      if parsed[:parsed]
+        add_words(parsed[:positions], id, name)
+        canonical = parsed[:canonical_name][:value]
+        canonical_uuid = UuidGenerator.generate(canonical)
+      end
+      @redis.set("ns:" + row[2], id)
+      [id, name, canonical_uuid, canonical, surrogate]
+    end
+
+    def add_words(pos, id, name)
+      pos.each do |e|
+        word = name[e[1]...e[2]]
+        entry = [word, id]
+        case e[0]
+        when 'uninomial'
+          @uninomial << entry
+        when 'genus'
+          @genus << entry
+        when 'specific_epithet'
+          @species << entry
+        when 'subspecific_epithet'
+          @subspecies << entry
+        when 'author_word'
+          @author << entry
+        when 'year'
+          @year << entry
+        end
+      end
+    end
+
+    def prepare_name_string_indices
+      processing_title("name_string_indices")
+      offset = 0
+      limit = 10000
+      while offset < 10_000 # 51_200_000
+        puts "name_string_indices rows so far: #{offset}"
+        res = @db.execute "SELECT data_source_id, name_string_id,
+                           url, taxon_id, global_id, local_id,
+                           nomenclatural_code_id, rank,
+                           accepted_taxon_id, classification_path,
+                           classification_path_ids,
+                           classification_path_ranks
+                           FROM name_string_indices
+                           limit #{limit} offset #{offset}"
+        offset += limit
+        while row = res.fetch do
+          entry = entry_name_string_indices(row)
+          @name_string_indices << entry
+        end
+        res.finish
+      end
+    end
+
+    def entry_name_string_indices(row)
+      [row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8],
+       row[9], row[10], row[11], nil, nil]
+    end
+
+    def prepare_vernacular_strings
+      processing_title("vernacular_strings")
+      offset = 0
+      limit = 100_000
+      while offset < 1_200_000
+        puts "vernacular_strings rows so far: #{offset}"
+        res = @db.execute "SELECT id, name
+                           FROM vernacular_strings
+                           limit #{limit} offset #{offset}"
+        offset += limit
+        while row = res.fetch do
+          entry = entry_vernacular(row)
+          @vernacular_strings << entry
+        end
+        res.finish
+      end
+    end
+
+    def entry_vernacular(row)
+      uuid = UuidGenerator.generate(row[1])
+      @redis.set("vn:" + row[0], uuid)
+      [uuid, row[1]]
+    end
+
+    def prepare_vernacular_string_indices
+      processing_title("vernacular_string_indices")
+      offset = 0
+      limit = 100_000
+      while offset < 2_500_000
+        puts "vernacular_strings_indices rows so far: #{offset}"
+        res = @db.execute "SELECT data_source_id, taxon_id,
+                           vernacular_string_id, language, locality,
+                           country_code
+                           FROM vernacular_string_indices
+                           limit #{limit} offset #{offset}"
+        offset += limit
+        while row = res.fetch do
+          entry = entry_vernacular_indices(row)
+          @vernacular_string_indices << entry
+        end
+        res.finish
+      end
+    end
+
+    def entry_vernacular_indices(row)
+      uuid = @redis.get("vn:" + row[2])
+      [row[0], row[1], uuid, row[3], row[4], row[5]]
+    end
+
+    private
+
+    def prepare_csv
+      data_source_file = File.expand_path(File.join(__dir__, '..',
+                                          '..', 'csv', 'data_sources.csv'))
+
+      @data_sources = CSV.open(data_source_file, "w:utf-8")
+      @data_sources << %w[id title description
+                          logo_url web_site_url data_url
+                          refresh_period_days name_strings_count
+                          data_hash unique_names_count created_at
+                          updated_at]
 
       name_string_file = File.expand_path(File.join(__dir__, '..',
                                           '..', 'csv', 'name_strings.csv'))
       @name_strings = CSV.open(name_string_file, "w:utf-8")
       @name_strings << %w[id name canonical_uuid canonical surrogate]
+
+      name_string_indices_file = File.expand_path(File.join(__dir__, '..',
+                                          '..', 'csv',
+                                          'name_string_indices.csv'))
+      @name_string_indices = CSV.open(name_string_indices_file, "w:utf-8")
+      @name_string_indices << %w[data_source_id name_string_id
+                                 url taxon_id global_id local_id
+                                 nomenclatural_code_id rank
+                                 accepted_taxon_id classification_path
+                                 classification_path_ids
+                                 classification_path_ranks accepted_name_uuid
+                                 accepted_name]
 
       authors_file = File.expand_path(File.join(__dir__, '..',
                                       '..', 'csv',
@@ -47,76 +238,30 @@ module Migrator
       uninomial_file = File.expand_path(File.join(__dir__, '..',
                                       '..', 'csv',
                                       'name_strings__uninomial.csv'))
-      @uninomial = CSV.open(subspecies_file, "w:utf-8")
+      @uninomial = CSV.open(uninomial_file, "w:utf-8")
       @uninomial << %w[uninomial name_uuid]
 
       year_file = File.expand_path(File.join(__dir__, '..',
                                       '..', 'csv',
                                       'name_strings__year.csv'))
-      @year = CSV.open(subspecies_file, "w:utf-8")
+      @year = CSV.open(year_file, "w:utf-8")
       @year << %w[year name_uuid]
 
-      offset = 0
-      limit = 100000
-      while offset < 25_000_000
-        puts "Rows so far: #{offset}"
-        res = @db.execute "SELECT name, surrogate
-                           FROM name_strings
-                           limit #{limit} offset #{offset}"
-        offset += limit
-        while row = res.fetch do
-          entry = prepare_entry(row)
-          @name_strings << entry
-        end
-        res.finish
-      end
-    end
+      vernacular_strings_file = File.expand_path(File.join(__dir__, '..',
+                                      '..', 'csv',
+                                      'vernacular_strings.csv'))
+      @vernacular_strings = CSV.open(vernacular_strings_file, "w:utf-8")
+      @vernacular_strings << %w[id name]
 
-    def prepare_entry(row)
-      name = row[0]
-      surrogate = row[1]
-      parsed = JSON.parse(@snp.fromString(name).renderCompactJson,
-                          symbolize_names: true)
-      id = parsed[:name_string_id]
-      canonical = canonical_uuid = nil
-      if parsed[:parsed]
-        add_words(parsed[:positions], id, name)
-        canonical = parsed[:canonical_name][:value]
-        parsed_canonical = JSON.parse(
-          @snp.fromString(canonical).renderCompactJson,
-          symbolize_names: true)
-        canonical_uuid = parsed_canonical[:name_string_id]
-      end
-      [id, name, canonical_uuid, canonical, surrogate]
-    end
+      vernacular_string_indices_file = File.expand_path(File.join(__dir__, '..',
+                                      '..', 'csv',
+                                      'vernacular_string_indices.csv'))
+      @vernacular_string_indices = CSV.open(vernacular_string_indices_file,
+                                            "w:utf-8")
+      @vernacular_string_indices << %w[data_source_id taxon_id
+                                       vernacular_string_id language
+                                       locality country_code]
 
-    def add_words(pos, id, name)
-      pos.each do |e|
-        word = name[e[1]...e[2]]
-        entry = [word, id]
-        case e[0]
-        when 'uninomial'
-          @uninomial << entry
-        when 'genus'
-          @genus << entry
-        when 'specific_epithet'
-          @species << entry
-        when 'subspecific_epithet'
-          @subspecies << entry
-        when 'author_word'
-          @author << entry
-        when 'year'
-          @year << entry
-        end
-      end
     end
-# {"name_string_id":"b0f8459f-8b73-514c-b6f3-568d54d99ded","parsed":true,"quality":1,"parser_version":"0.4.1","verbatim":"Salinator
-# so lida (Martens, 1878)","normalized":"Salinator solida (Martens
-# 1878)","canonical_name":{"value":"Salinator solida"},"hybrid":false,"s
-# urrogate":false,"virus":false,"bacteria":false,"details":[{"genus":{"value":"Salinator"},"specific_epithet":{"value":"solida","autho
-#  rship":{"value":"(Martens
-#                    1878)","basionym_authorship":{"authors":["Martens"],"year":{"value":"1878"}}}}}],"positions":[["genus",0,9
-# ],["specific_epithet",10,16],["author_word",18,25],["year",27,31]]}
-# """
   end
 end
